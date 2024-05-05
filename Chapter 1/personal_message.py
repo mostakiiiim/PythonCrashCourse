b23b3b4543136c44ddc9784ba6f7c5434272f5ba
Mostakim Reza Omey <mostakiiiim@gmail.com>
2024-05-05 23:50:54 +0600

    String

--- Chapter 1/personal_message.py
+++ Chapter 1/personal_message.py
@@ -1,3 +1,5 @@
+# TryItYourself
+
 name = "Eric"
 
 message = f"Hello {name}, would you like to learn some Python today? "
@@ -7,3 +9,9 @@ print(message)
 print(name.upper())
 print(name.lower())
 print(name.title())
+
+author = 'Charles Bukowski'
+
+quote = "If you're going to try, go all the way. Otherwise, don't even start."
+
+print(f'{author} once said, "{quote}"')

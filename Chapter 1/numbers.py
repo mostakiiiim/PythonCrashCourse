c786985fc3996314497e5cf9220f5834ad1036df
Mostakim Reza Omey <mostakiiiim@gmail.com>
2024-05-17 00:30:45 +0200

    numbers

---
+++ Chapter 1/numbers.py
@@ -0,0 +1,22 @@
+# Numbers in Python
+# two multiplication in python will indicate a exponentiation
+
+print(2*2)
+print(3**2)
+# floats
+
+# any number with decimal point in python will be float
+
+# if you any two numbers it will result in float
+
+print(2/2)
+
+# if you mix integer and float it will result float
+
+print(2*2.0)
+
+# underscore can be used to demonstrate a large number readable
+
+monthly_expense = 100_00_000_000
+
+print(monthly_expense)

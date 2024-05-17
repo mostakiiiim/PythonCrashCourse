f9013915f26070b0a1db0458323d84658c01eb2e
Mostakim Reza Omey <mostakiiiim@gmail.com>
2024-05-18 01:48:11 +0200

    list

---
+++ Chapter 3/ex3.1.py
@@ -0,0 +1,10 @@
+friends_names = ['sajid', 'sunny', 'iftekhar', 'zuhayer']
+
+print(friends_names[0])
+print(friends_names[1])
+print(friends_names[2])
+print(friends_names[3])
+
+greetings = f'Meet my good friend {friends_names[0]}'
+
+print(greetings)
---
+++ Chapter 3/list.py
@@ -0,0 +1,7 @@
+cities = ['munich', 'ingolstadt', 'hamm', 'nuremburg']
+
+print(cities)
+
+print(cities[1])
+
+print(cities[1].title())
